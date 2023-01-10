d10cdde8ed68cfb89541d99d43a7e359e6354620
DSTIXX)% <delightsomedunsiasolo24@gmail.com>
2023-01-10 15:21:24 -0800

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 6-from_json_string.py
+"""Defines a JSON-to-object function."""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
